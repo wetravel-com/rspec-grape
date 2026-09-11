c592c2fa07a41603afd850ff0f4dea4a80acef3a
Serge Dubrovin <serge.dubrovin@wetravel.com>
2026-09-11 08:43:01 +0200

    Remove comment

--- lib/wt-rspec-grape.rb
+++ lib/wt-rspec-grape.rb
@@ -1,11 +1,6 @@
 require 'rspec/core'
 require 'rack/test'
 require 'grape'
-# Defines Grape::Endpoint.before_each / .reset_before_each, which the endpoint
-# matchers use. Documented as opt-in all along, but Grape < 4 left it in the
-# eager load so `require 'grape'` pulled it in; Grape >= 4 excludes it. Must come
-# after `require 'grape'` -- it prepends onto Grape::Endpoint at load time. A
-# no-op on Grape 3.3, where the eager load got there first.
 require 'grape/testing'
 
 require 'rspec/grape/exceptions'
